a998d30b3f060faff4496b74c992da2f6d0f6b89
Alex Anderson <alex@alwian.com>
2020-12-09 17:40:44 +0000

    Implemented Merge Sort

--- assets/js/algorithms.js
+++ assets/js/algorithms.js
@@ -69,3 +69,47 @@ function quickSort(array, start, end) {
         quickSort(array, lPointer, end)
     }
 }
+
+function mergeSort(array) {
+    if (array.length <= 1) {
+        return array;
+    }
+
+    let left = [];
+    let right = [];
+
+    for (let x = 0; x < array.length; x++) {
+        if (x < Math.floor(array.length / 2)) {
+            left.push(array[x]);
+        } else {
+            right.push(array[x]);
+        }
+    }
+
+    left = mergeSort(left);
+    right = mergeSort(right);
+
+    return merge(left, right);
+}
+
+function merge(left, right) {
+    let result = [];
+
+    while (left.length > 0 && right.length > 0) {
+        if (left[0] <= right[0]) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+
+    while (left.length > 0) {
+        result.push(left.shift());
+    }
+
+    while (right.length > 0) {
+        result.push(right.shift());
+    }
+
+    return result;
+}
