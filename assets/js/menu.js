138cbf5793c8a5e6b1b30358d6e306b071a9a680
Alex Anderson <alex@alwian.com>
2020-12-09 18:58:36 +0000

    Disable buttons while sorting

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -7,8 +7,23 @@ let sortTypes = document.getElementsByClassName("sort-type");
 // Following code is used to highlight the selected algorithm.
 for (let i = 0; i < sortTypes.length; i++) {
     sortTypes[i].addEventListener("click", function () {
-        sortTypes[selectedSort].className = "sort-type";
-        selectedSort = i;
-        sortTypes[i].className = sortTypes[i].className + " selected-sort";
+        if (sortTypes[i].classList.contains("enabled-btn")) {
+            sortTypes[selectedSort].className = "sort-type enabled-btn";
+            selectedSort = i;
+            sortTypes[i].className = sortTypes[i].className + " selected-sort";
+        }
     });
+}
+
+function toggleButtons(enabled) {
+    for (let x = 0; x < sortTypes.length; x++) {
+        sortTypes[x].classList.add(enabled ? "enabled-btn" : "disabled-btn");
+        sortTypes[x].classList.remove(enabled ? "disabled-btn" : "enabled-btn");
+    }
+
+    sortBtn.classList.add(enabled ? "enabled-btn" : "disabled-btn");
+    sortBtn.classList.remove(enabled ? "disabled-btn" : "enabled-btn");
+    refreshBtn.classList.add(enabled ? "enabled-btn" : "disabled-btn");
+    refreshBtn.classList.remove(enabled ? "disabled-btn" : "enabled-btn");
+
 }
